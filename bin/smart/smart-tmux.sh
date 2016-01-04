5ac7832666ec70e43bf0d2b643201d8bed48c05d
Marcin Bednarek <paws77@gmail.com>
2016-01-04 13:23:46 +0100

    changed root dir.

--- bin/smart/smart-tmux.sh
+++ bin/smart/smart-tmux.sh
@@ -3,19 +3,19 @@ set -vx
 tmux new-session -d -s smart 
 tmux new-window -t smart:0
 tmux new-window -t smart:1
-tmux send-keys -t smart:1 'gosmart' C-m
+tmux send-keys -t smart:1 'gosmart1' C-m
 
 tmux new-window -t smart:2
-tmux send-keys -t smart:2 'gosmart' C-m
+tmux send-keys -t smart:2 'gosmart1' C-m
 
 tmux new-window -t smart:3
-tmux send-keys -t smart:3 'gosmart' C-m
+tmux send-keys -t smart:3 'gosmart2' C-m
 
 tmux new-window -t smart:4
-tmux send-keys -t smart:4 'gosmart' C-m
+tmux send-keys -t smart:4 'gosmart2' C-m
 
 tmux new-window -t smart:5
-tmux send-keys -t smart:5 'gosmart' C-m
+tmux send-keys -t smart:5 'gosmart1' C-m
 tmux send-keys -t smart:5 'foreman start' C-m
 
 tmux new-window -t smart:6
