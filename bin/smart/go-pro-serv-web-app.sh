71df9e01d437fc864cd4e0fbfd10ae6b2dc22941
Marcin Bednarek <paws77@gmail.com>
2016-03-10 06:48:51 +0100

    renamed scripts.

--- bin/smart/go-pro-serv-web-app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd $SMART
-SBT_OPTS="$SBT_OPTS -Xdebug -Xnoagent -Dhttp.netty.maxHeaderSize=16384 -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4002,server=y,suspend=n"
-sbt "project pro-serv-web-app" "run 9001"
--- bin/smart/go-workday-smart-webapp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd $SMART
-#export SBT_OPTS="$SBT_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4000,server=y,suspend=n"
-SBT_OPTS="$SBT_OPTS -Xdebug -Xnoagent -Dhttp.netty.maxHeaderSize=16384 -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4000,server=y,suspend=n"
-sbt "project workday-smart-webapp" "run 9000"
