4b811a793df200e27eaef319fccade3b258b84de
Marcin Bednarek <paws77@gmail.com>
2016-03-13 22:44:08 +0100

    2 idea instances.

--- bin/smart/smart-tmux.sh
+++ bin/smart/smart-tmux.sh
@@ -16,12 +16,14 @@ tmux send-keys -t smart:4 'gosmart2' C-m
 
 tmux new-window -t smart:5
 tmux send-keys -t smart:5 'gosmart1' C-m
-tmux send-keys -t smart:5 'foreman start' C-m
+tmux send-keys -t smart:5 'start-smart1.sh' C-m
 
 tmux new-window -t smart:6
 tmux send-keys -t smart:6 'godatabase' C-m
 
 tmux new-window -t smart:7
+
+tmux new-window -t smart:8
 
 tmux select-window -t smart:0
 tmux -2 attach-session -t smart
--- bin/smart/start-idea2.sh
+++ bin/smart/start-idea2.sh
@@ -3,4 +3,4 @@ source ~/.bashrc
 if [ -f "${HOME}/.bash_profile_smart2" ]; then
   source "${HOME}/.bash_profile_smart2"
 fi
-"/Applications/IntelliJ IDEA 15 CE.app/Contents/MacOS/idea"
+"/Applications/IntelliJ IDEA 15 CE2.app/Contents/MacOS/idea"
