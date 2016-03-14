b6b64bc18b2a4014f27cb0f0c283fc73f1c80e8b
Marcin Bednarek <paws77@gmail.com>
2016-03-14 10:03:19 +0100

    names for tabs.

--- bin/smart/smart-tmux.sh
+++ bin/smart/smart-tmux.sh
@@ -1,29 +1,29 @@
 #!/bin/sh 
 set -vx
 tmux new-session -d -s smart 
-tmux new-window -t smart:0
-tmux new-window -t smart:1
+tmux new-window -n 'main' -t smart:0
+tmux new-window -n 'smart1' -t smart:1
 tmux send-keys -t smart:1 'gosmart1' C-m
 
-tmux new-window -t smart:2
+tmux new-window -n 'smart1' -t smart:2
 tmux send-keys -t smart:2 'gosmart1' C-m
 
-tmux new-window -t smart:3
+tmux new-window -n 'smart2' -t smart:3
 tmux send-keys -t smart:3 'gosmart2' C-m
 
-tmux new-window -t smart:4
+tmux new-window -n 'smart2' -t smart:4
 tmux send-keys -t smart:4 'gosmart2' C-m
 
-tmux new-window -t smart:5
+tmux new-window -n 'startsmart1' -t smart:5
 tmux send-keys -t smart:5 'gosmart1' C-m
 tmux send-keys -t smart:5 'start-smart1.sh' C-m
 
-tmux new-window -t smart:6
+tmux new-window -n 'database' -t smart:6
 tmux send-keys -t smart:6 'godatabase' C-m
 
-tmux new-window -t smart:7
+tmux new-window -n 'idea1' -t smart:7
 
-tmux new-window -t smart:8
+tmux new-window -n 'idea2' -t smart:8
 
 tmux select-window -t smart:0
 tmux -2 attach-session -t smart
