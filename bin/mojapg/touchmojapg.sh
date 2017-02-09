bb1df3986d8a8e62f8da9b8fad4184f15607fa6f
Marcin Bednarek <paws77@gmail.com>
2017-02-09 08:59:29 +0100

    Changed directory to mojapg.

--- bin/mojapg/touchmojapg.sh
+++ bin/mojapg/touchmojapg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd $PROJECTS/mojapg
+cd $PROJECTS/mojapg/trunk/vagrant
 touch docker-run/mojapg-business-trunk/srv/config/docker/deploy.yes
 touch docker-run/mojapg-portal-trunk/srv/config/docker/deploy.yes
