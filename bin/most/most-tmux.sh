0cc376c9c43d1b7405ded7cc216ee99235e664cc
Marcin Bednarek <paws77@gmail.com>
2019-06-10 09:53:53 +0200

    Zmiany tmux.

--- bin/most/most-tmux.sh
+++ bin/most/most-tmux.sh
@@ -1,26 +1,26 @@
 #!/bin/sh 
 set -vx
-tmux new-session -d -s most 
-tmux new-window -n 'main' -t most:0
+tmux new-session -d -s most -n 'main'
+#tmux new-window -n 'main' -t most:0
 tmux new-window -n 'business' -t most:1
-tmux send-keys -t most:1 'gomostbusiness' C-m
+tmux send-keys -t most:1 'gomostbusiness' Enter
 
 tmux new-window -n 'importer' -t most:2
-tmux send-keys -t most:2 'gomostimporterbusiness' C-m
+tmux send-keys -t most:2 'gomostimporterbusiness' Enter
 
 tmux new-window -n 'connector' -t most:3
-tmux send-keys -t most:3 'gomostexternalconnectorbusiness' C-m
+tmux send-keys -t most:3 'gomostexternalconnectorbusiness' Enter
 
 tmux new-window -n 'aggregator' -t most:4
-tmux send-keys -t most:4 'gomostaggregatorbusiness' C-m
+tmux send-keys -t most:4 'gomostaggregatorbusiness' Enter 
 
 tmux new-window -n 'logs' -t most:5
-tmux send-keys -t most:5 'tailall' C-m
+tmux send-keys -t most:5 'tailall' Enter
 
 tmux new-window -n 'elastic' -t most:6
 
 tmux new-window -n 'mojapg' -t most:7
-tmux send-keys -t most:7 'gomojapg' C-m
+tmux send-keys -t most:7 'gomojapg' Enter
 
 tmux new-window -n 'idea2' -t most:8
 
