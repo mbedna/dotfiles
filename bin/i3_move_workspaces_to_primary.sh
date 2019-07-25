638324da2b4e95a50cb0f0fc47ff20d2421de653
Marcin Bednarek <paws77@gmail.com>
2019-07-25 09:40:25 +0200

    skrypty.

---
+++ bin/i3_move_workspaces_to_primary.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+i3-msg '[workspace="2:dev"]' move workspace to output eDP-1
+i3-msg '[workspace="3:web"]' move workspace to output eDP-1
+i3-msg '[workspace="4:term"]' move workspace to output eDP-1
