9ffe70ede86512e2e8a7775633d87b5a25d93f48
Marcin Bednarek <paws77@gmail.com>
2020-08-26 13:11:01 +0200

    przełączanie javy.

---
+++ bin/java11.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export JAVA_HOME=/opt/tools/jdk-11.0.2
+export PATH=$JAVA_HOME/bin:$PATH
+echo $JAVA_HOME
+java -version
---
+++ bin/java14.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export JAVA_HOME=/opt/tools/jdk-14
+export PATH=$JAVA_HOME/bin:$PATH
+echo $JAVA_HOME
+java -version
+
